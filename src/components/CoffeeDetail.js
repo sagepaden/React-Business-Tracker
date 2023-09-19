fd70bea339b10b013477ba349e95ee11cc300afa
sagepaden <sagepaden@gmail.com>
2023-09-19 08:35:04 -0700

    deleting commented out code

--- src/components/CoffeeDetail.js
+++ src/components/CoffeeDetail.js
@@ -36,39 +36,3 @@ CoffeeDetail.propTypes = {
 };
 
 export default CoffeeDetail;
-
-
-  // handleBuyCoffeeClick = () => {
-  //   if (this.state.quantity === 0) {
-  //     return;
-  //   }
-  //   this.setState((prevState) => ({
-  //       quantity: prevState.quantity - 1
-  //   }));
-  // };
-
-//   render() {
-//     const { coffee, onClickingDelete, onClickingEdit } = this.props;
-//     return (
-//       <React.Fragment>
-//         <Card style={{ width: '18rem'}}>
-//           <Card.Body>
-//             <Card.Title>{coffee.name}</Card.Title>
-//             <Card.Subtitle className="mb-2 text-muted">{coffee.roast}</Card.Subtitle>
-//             <Card.Text>
-//               <ul>
-//                 <li>Origin | {coffee.origin}</li>
-//                 <li>Price | ${coffee.price}</li>
-//                 <li>Quantity | {this.state.quantity}</li>
-//               </ul>
-//             </Card.Text>
-//           </Card.Body>
-//         </Card>
-//         <button className="btn btn-success" onClick={this.handleBuyCoffeeClick}>Buy Coffee</button>
-//         <button className="btn btn-success" onClick={()=> onClickingEdit(coffee.id) }>Edit Coffee</button>
-//         <button className="btn btn-success" onClick={()=> onClickingDelete(coffee.id) }>Delete Coffee</button>
-//         <hr/>
-//       </React.Fragment>
-//     );
-//   }
-// }
